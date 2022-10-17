d7de128b81d78a17f918e79a75e390a8f9a2eccd
Kate Johnson <katelouisejohnson@gmail.com>
2022-10-17 16:07:18 +0200

    Update POST EP to handle multipart form

--- run.py
+++ run.py
@@ -1,10 +1,14 @@
-from fastapi import FastAPI
+from typing import List
+from fastapi import Form, File, UploadFile, FastAPI
 from models import Property
 from database import setup_database, get_connection, insert_property
+import json
 
 setup_database()
+# Note: not entirely sure if creating once connection to persist throughout is safe
 database=get_connection()
 app = FastAPI()
+# TODO: Auth!
 
 @app.get('/')
 def home():
@@ -15,14 +19,18 @@ def ping():
     return 'pong'
 
 @app.post('/properties')
-def save_property(property: Property):
-    # TODO:
-    # deal with image and pdf files
+def save_property(
+    form: str = Form(),
+    files: List[UploadFile] = File(...)
+):
     status=''
     message=''
-    
+    property = Property(**json.loads(form))
+
+    # TODO: create a controller and/or service
     try:
         id = insert_property(database, database.cursor(), property)
+        # TODO: handle file storage
         message= 'ID: ' + id
         status='success'
     except Exception as error:
